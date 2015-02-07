e45fed416b3e28b9010c0dcee1564bf3fe4589a8
Jack Daniel Harding <jackdanielharding@gmail.com>
2015-02-07 15:25:54 +0000

    Created class diagram for Player.cs

---
+++ ShootEmUp/ShootEmUp/ShootEmUp.Windows/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShootEmUp
+{
+    public class Player
+    {
+        public Player()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private int x
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        private int y
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int getX()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int getY()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
