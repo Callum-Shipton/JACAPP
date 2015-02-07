6cff92d898edf2b75e075ab2834c47d8b3d3dbc6
Jack Daniel Harding <jackdanielharding@gmail.com>
2015-02-07 16:29:07 +0000

    Entity code edited to allow movement

--- ShootEmUp/ShootEmUp/ShootEmUp.Windows/Player.cs
+++ ShootEmUp/ShootEmUp/ShootEmUp.Windows/Player.cs
@@ -8,8 +8,8 @@ namespace ShootEmUp
 {
     class Player : Entity
     {
-        public Player(int x, int y, int health)
-            : base(x, y, health)
+        public Player(int x, int y, int speed, int health)
+            : base(x, y, speed, health)
         {
             
         }
