fc12754de24d2e8f270dadfa0da519b12af641c2
Jack Daniel Harding <jackdanielharding@gmail.com>
2015-02-07 18:56:49 +0000

    Added Abstact class Entity.class and subclass Player.Class

---
+++ ShootEmUp/src/Entity.java
@@ -0,0 +1,61 @@
+
+public abstract class Entity {
+	
+	private int posX;
+	private int posY;
+	private int speed;
+	private int health;
+	
+	//Constructors
+	
+	public Entity(int posX, int posY, int speed, int health){
+		this.posX = posX;
+		this.posY = posY;
+		this.speed = speed;
+		this.health = health;
+	}
+	
+	//Methods
+	
+	public void moveHorizontally(int direction){
+		posX += (speed * direction);
+	}
+
+	public void moveVertically(int direction){
+		posY += (speed * direction);
+	}
+	
+	//Setters and getters
+	
+	public int getX(){
+		return posX;
+	}
+	
+	public void setX(int posX){
+		this.posX = posX;
+	}
+	
+	public int getY(){
+		return posY;
+	}
+	
+	public void setY(int posY){
+		this.posY = posY;
+	}
+	
+	public int getSpeed(){
+		return speed;
+	}
+	
+	public void setSpeed(int speed){
+		this.speed = speed;
+	}
+	
+	public int getHealth(){
+		return health;
+	}
+	
+	public void setHealth(int health){
+		this.health = health;
+	}
+}
---
+++ ShootEmUp/src/Player.java
@@ -0,0 +1,11 @@
+
+public class Player extends Entity{
+	
+	public Player(){
+		super(10, 10, 10, 10);
+	}
+	
+	public Player(int posX, int posY, int speed, int health){
+		super(posX, posY, speed, health);
+	}
+}
