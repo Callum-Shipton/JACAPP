28414a879eb17cbbab70f42ecd80bb35e79758af
Jack Daniel Harding <jackdanielharding@gmail.com>
2015-02-11 23:31:41 +0000

    added render method to Player.class

--- ShootEmUp/src/Object/Player.java
+++ ShootEmUp/src/Object/Player.java
@@ -27,6 +27,10 @@ public class Player extends Entity {
 		deathCheck();
 	}
 	
+	public void render() {
+		// TODO render stuff
+	}
+	
 	private void checkKeys() {
 		if (Keyboard.getKey(GLFW_KEY_W) == 1
 				|| Keyboard.getKey(GLFW_KEY_W) == 2) {
