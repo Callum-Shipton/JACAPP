38dbc8adee333e8dbd7b431d91f8262d07c21e9e
Jack Daniel Harding <jackdanielharding@gmail.com>
2015-02-11 01:22:17 +0000

    Added Particle image

--- ShootEmUp/src/Art.java
+++ ShootEmUp/src/Art.java
@@ -4,5 +4,7 @@ public class Art {
 	public static String face = "res/img/awesomeface.png";
 	public static String player = "res/img/Player.png";
 	
+	public static String particle = "res/img/Particle.png";
+	
 	public static String level1 = "res/levels/level1.png";
 }
