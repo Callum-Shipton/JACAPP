c052b15b12c5cf334bde89d2fd60e8f76e66d8b7
Jack Daniel Harding <jackdanielharding@gmail.com>
2015-02-12 17:23:49 +0000

    changes sizes

--- ShootEmUp/src/Object/Particle.java
+++ ShootEmUp/src/Object/Particle.java
@@ -49,7 +49,7 @@ public class Particle extends Entity implements Collidable{
 	}
 	
 	public void render(Renderer r){
-		r.draw(Art.particleID,new Vector2((float)(posX + 25), (float)(posY + 25)),new Vector2(50.0f, 50.0f), 0.0f, new Vector2(1.0f,(float)direction), new Vector2(1.0f,8.0f));
+		r.draw(Art.particleID,new Vector2((float)(posX + 16), (float)(posY + 16)),new Vector2(32.0f, 32.0f), 0.0f, new Vector2(1.0f,(float)direction), new Vector2(1.0f,8.0f));
 	}
 	
 	@Override
