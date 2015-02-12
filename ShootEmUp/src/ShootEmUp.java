a8f47e12012e9c0767cf62b7a90fa2c51308e9cb
Callum Shipton <callumshipton@msn.com>
2015-02-12 13:31:22 +0000

    Not a single pixel big

--- ShootEmUp/src/ShootEmUp.java
+++ ShootEmUp/src/ShootEmUp.java
@@ -80,10 +80,12 @@ public class ShootEmUp {
 	        
 			GL13.glActiveTexture(GL13.GL_TEXTURE0);
 	        GL11.glBindTexture(GL11.GL_TEXTURE_2D, d.textureIDs[0]);
+	        
+	        level1.render();
 
 		glfwSwapBuffers(d.getWindow()); // Swaps front and back buffers to render changes
 		
-		level1.render();
+		
 	}
 
 	public static void main(String[] args){
