cb49bfddba573eb387f3f81eb7b73bd9012a7543
Jack Daniel Harding <jackdanielharding@gmail.com>
2015-02-12 16:37:14 +0000

    Some pushes

--- ShootEmUp/src/Object/Particle.java
+++ ShootEmUp/src/Object/Particle.java
@@ -48,7 +48,7 @@ public class Particle extends Entity implements Collidable{
 	}
 	
 	public void render(Renderer r){
-		r.draw(0,new Vector2((float)posX, (float)posY),new Vector2(100.0f, 100.0f), (float)direction*45, new Vector2(1.0f,1.0f), new Vector2(1.0f,1.0f));
+		r.draw(0,new Vector2((float)posX, (float)posY),new Vector2(100.0f, 100.0f), 0.0f, new Vector2(1.0f,(float)direction), new Vector2(1.0f,8.0f));
 	}
 	
 	@Override
