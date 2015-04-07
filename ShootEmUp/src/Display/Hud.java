d53c743e8627400e6c52d0bc6dcdd6d60d6e05db
Callum Shipton <callumshipton@msn.com>
2015-04-07 14:32:10 +0100

    Hud now implements smaller bars for smaller max health.

--- ShootEmUp/src/Display/Hud.java
+++ ShootEmUp/src/Display/Hud.java
@@ -13,24 +13,20 @@ public class Hud {
 	private HudElement xpBar;
 	private HudElement moneyCounter;
 	private Player player;
+	private float hudW;
 
 	public Hud(Player player){
 		this.player = player;
-		
 		hudElems = new CopyOnWriteArrayList<HudElement>();
 		infoBox = new HudElement(0.0f,0.0f,189.0f,110.0f, Art.infoBox);
 		hudElems.add(infoBox);
-		healthBar = new HudElement(10.0f, 10.0f, 170.0f, 19.0f, Art.healthBar);
+		healthBar = new HudElement(10.0f, 10.0f, Art.healthBar.getWidth(), Art.healthBar.getHeight()/Art.healthBar.getFHeight(), Art.healthBar);
 		hudElems.add(healthBar);
-		healthBar.setVal(18);
-		healthBar.setMFrame(1.0f, Art.healthBar.getFHeight() );
-		manaBar = new HudElement(10.0f, 35.0f, 170.0f, 19.0f, Art.manaBar);
+		manaBar = new HudElement(10.0f, 35.0f, Art.manaBar.getWidth(), Art.manaBar.getHeight()/Art.manaBar.getFHeight(), Art.manaBar);
 		hudElems.add(manaBar);
-		manaBar.setVal(18);
-		manaBar.setMFrame(1.0f, Art.manaBar.getFHeight() );
-		xpBar = new HudElement(10.0f, 60.0f, 170.0f, 19.0f, Art.xpBar);
+		xpBar = new HudElement(10.0f, 60.0f, Art.xpBar.getWidth(), Art.xpBar.getHeight()/Art.xpBar.getFHeight(), Art.xpBar);
 		hudElems.add(xpBar);
-		moneyCounter = new HudElement(10.0f, 85.0f, 18.0f, 18.0f, Art.BarCoin);
+		moneyCounter = new HudElement(10.0f, 85.0f, Art.BarCoin.getWidth(), Art.BarCoin.getHeight(), Art.BarCoin);
 		hudElems.add(moneyCounter);
 	}
 	
@@ -41,8 +37,14 @@ public class Hud {
 	}
 
 	public void update() {
+		hudW = (Math.max(player.getMaxHealth(), player.getMaxMana()) - 18.0f)*10.0f + 189.0f;
+		infoBox.setSize(hudW, 110.0f);
 		healthBar.setVal(player.getHealth());
+		healthBar.setSize(player.getMaxHealth()*Art.healthBar.getWidth()/((Art.healthBar.getHeight()/Art.healthBar.getFHeight())-1), Art.healthBar.getHeight()/Art.healthBar.getFHeight());
+		healthBar.setMFrame(18.0f/player.getMaxHealth(), Art.healthBar.getFHeight() );
 		manaBar.setVal(player.getMana());
+		manaBar.setSize(player.getMaxMana()*Art.manaBar.getWidth()/((Art.manaBar.getHeight()/Art.manaBar.getFHeight())-1), Art.manaBar.getHeight()/Art.manaBar.getFHeight());
+		manaBar.setMFrame(18.0f/player.getMaxMana(), Art.manaBar.getFHeight() );
 		//healthBar.setMFrame((player.getMaxHealth()+1)/(player.getHealth()+1), Art.healthBar.getFHeight());
 		//healthBar.setSize((((float)player.getHealth())/((float)player.getMaxHealth())) * Art.healthBar.getWidth(), healthBar.getSize().y());
 	}
