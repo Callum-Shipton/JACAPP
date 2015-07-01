c875d061a9e626bf10a5e400d31cf51204212d94
Jack Daniel Harding <jackdanielharding@gmail.com>
2015-07-01 15:29:59 +0100

    Fixed pause menu tabs

--- ShootEmUp/src/GUI/Menus/MagicMenu.java
+++ ShootEmUp/src/GUI/Menus/MagicMenu.java
@@ -11,23 +11,23 @@ import GUI.Button;
 import Input.Keyboard;
 import Main.ShootEmUp;
 
-public class InventoryMenu extends GuiMenu {
+public class MagicMenu extends GuiMenu {
 	
 	static int selectedItem = 0;
 	public static boolean saved;
 	private Button back;
 	private Button exit;
+	private Button invButton;
 	private Button skillButton;
-	private Button magicButton;
 	private Button mapButton;
 	private Button saveButton;
 
-    public InventoryMenu(Image menuImage) {
+    public MagicMenu(Image menuImage) {
         super(menuImage);
         selectedItem = 0;
         back = addButton(new Button(Art.backButton, 30, 30, 128,24));
         exit = addButton(new Button(Art.exitButton, 30, 64, 128,24));
-        magicButton = addButton(new Button(Art.magicButton, 922, 102, 101, 102));
+        invButton = addButton(new Button(Art.invButton, 922, 0, 101, 102));
         skillButton = addButton(new Button(Art.skillButton, 922, 204, 101, 102));
         mapButton = addButton(new Button(Art.mapButton, 922, 306, 101, 102));
         saveButton = addButton(new Button(Art.saveButton, 922, 408, 101, 102));
@@ -41,9 +41,9 @@ public class InventoryMenu extends GuiMenu {
 
     public void update() {
     	super.update();
-    	if(magicButton.hasClicked()){
-    	    addMenu(new MagicMenu(Art.magicScreen));
-    	    magicButton.postAction();
+    	if(invButton.hasClicked()){
+    	    addMenu(new InventoryMenu(Art.invScreen));
+    	    invButton.postAction();
     	}
     	if(skillButton.hasClicked()){
     	    addMenu(new SkillMenu(Art.skillScreen));
