3cf624a15ca4a42ea3f5239353648434cc1397d7
Jack Daniel Harding <jackdanielharding@gmail.com>
2016-03-21 13:41:18 +0000

    -Added skill points counter to skills  menu -Added cost displays to skill upgrades in skills menu

--- ShootEmUp/src/gui/menus/SkillMenu.java
+++ ShootEmUp/src/gui/menus/SkillMenu.java
@@ -2,16 +2,43 @@ package gui.menus;
 
 import gui.Button;
 import gui.ButtonType;
+import gui.Counter;
+import main.ShootEmUp;
+import math.Vector2;
+import components.TypeComponent;
+import components.inventory.PlayerInventory;
 import display.Art;
 import display.Image;
 
 public class SkillMenu extends PauseMenu {
 
+	Counter skillPoints;
+	
     public SkillMenu(Image menuImage) {
         super(menuImage);
 		addButton(new Button(ButtonType.HEALTH_REGEN, Art.healthRegenButton,30, 98));
 		addButton(new Button(ButtonType.HEALTH, Art.healthButton,30, 132));
 		addButton(new Button(ButtonType.MANA_REGEN, Art.manaRegenButton,30, 166));
 		addButton(new Button(ButtonType.MANA, Art.manaButton,30, 200));
+		skillPoints = new Counter(30.0f, 234.0f, (Art.coin.getWidth() / Art.coin.getFWidth()), Art.coin.getHeight(), Art.coin, true);
+    }
+    
+    public void update(){
+    	super.update();
+    	
+    	skillPoints.update(((PlayerInventory)ShootEmUp.currentLevel.getPlayer().getComponent(TypeComponent.INVENTORY)).getLevelPoints());
+    }
+    
+    public void render(){
+    	super.render();
+    	skillPoints.render(Art.stat);
+		
+    	Vector2 size = new Vector2(16,16);
+		Vector2 maxTex = new Vector2(10,1);
+    	
+		Art.stat.draw(Art.numbers, new Vector2(160,103), size, 0.0f, new Vector2(1,1), maxTex);
+    	Art.stat.draw(Art.numbers, new Vector2(160,137), size, 0.0f, new Vector2(1,1), maxTex);
+    	Art.stat.draw(Art.numbers, new Vector2(160,171), size, 0.0f, new Vector2(1,1), maxTex);
+    	Art.stat.draw(Art.numbers, new Vector2(160,205), size, 0.0f, new Vector2(1,1), maxTex);
     }
 }
--- ShootEmUp/src/gui/menus/UpgradesMenu.java
+++ ShootEmUp/src/gui/menus/UpgradesMenu.java
@@ -13,24 +13,24 @@ import display.Image;
 
 public class UpgradesMenu extends PauseMenu {
 
-	Counter coin;
+	Counter coins;
 	
     public UpgradesMenu(Image menuImage) {
         super(menuImage);
         addButton(new Button(ButtonType.INVENTORY_UPGRADE, Art.inventoryButton, 30, 98));
 		addButton(new Button(ButtonType.POTIONS_UPGRADE, Art.potionsButton, 30, 132));
-		coin = new Counter(30.0f, 166.0f, (Art.coin.getWidth() / Art.coin.getFWidth()), Art.coin.getHeight(), Art.coin, true);
+		coins = new Counter(30.0f, 166.0f, (Art.coin.getWidth() / Art.coin.getFWidth()), Art.coin.getHeight(), Art.coin, true);
     }
     
     public void update(){
     	super.update();
     	
-    	coin.update(((PlayerInventory)ShootEmUp.currentLevel.getPlayer().getComponent(TypeComponent.INVENTORY)).getCoins());
+    	coins.update(((PlayerInventory)ShootEmUp.currentLevel.getPlayer().getComponent(TypeComponent.INVENTORY)).getCoins());
     }
     
     public void render(){
     	super.render();
-    	coin.render(Art.stat);
+    	coins.render(Art.stat);
 		
     	Vector2 size = new Vector2(16,16);
 		Vector2 maxTex = new Vector2(10,1);
