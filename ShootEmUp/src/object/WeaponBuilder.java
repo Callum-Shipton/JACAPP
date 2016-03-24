51e11ddfd70e6d00ede7ba914591e2aa3b482ae6
Jack Daniel Harding <jackdanielharding>
2016-03-24 14:11:07 +0000

    -changed weapon ranges for new range system

--- ShootEmUp/src/object/WeaponBuilder.java
+++ ShootEmUp/src/object/WeaponBuilder.java
@@ -8,15 +8,15 @@ public abstract class WeaponBuilder {
 	public static Weapon buildWeapon(TypeWeapon type, int team){
 		switch(type){
 		case SWORD:
-			return new Weapon(type, 1, 3, 5, true, 1, null, team, Art.swordProjectile, Art.swordButton);
+			return new Weapon(type, 1, 1, 5, true, 1, null, team, Art.swordProjectile, Art.swordButton);
 		case BATTLEAXE:
-			return new Weapon(type, 2, 2, 10, true, 2, null, team, Art.swordProjectile, Art.battleaxeButton);
+			return new Weapon(type, 2, 1, 10, true, 2, null, team, Art.swordProjectile, Art.battleaxeButton);
 		case MACE:
-			return new Weapon(type, 3, 2, 15, true, 3, null, team, Art.swordProjectile, Art.maceButton);
+			return new Weapon(type, 3, 1, 15, true, 3, null, team, Art.swordProjectile, Art.maceButton);
 		case CROSSBOW:
-			return new Weapon(type, 3, 3, 10, false, 1, null, team, Art.arrow, Art.crossbowButton);
+			return new Weapon(type, 3, 5, 10, false, 1, null, team, Art.arrow, Art.crossbowButton);
 		case BOW:
-			return new Weapon(type, 2, 2, 5, false, 1, null, team, Art.arrow, Art.bowButton);
+			return new Weapon(type, 2, 5, 5, false, 1, null, team, Art.arrow, Art.bowButton);
 		case FIRE_STAFF:
 			return new Weapon(type, 1, 3, 5, false, 1, Element.FIRE, team, Art.fireMagic, Art.fireStaffButton);
 		case ICE_STAFF:
